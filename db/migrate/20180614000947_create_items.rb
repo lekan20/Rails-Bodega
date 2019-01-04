cbdff703671ad1c08b184f2c8f6bf8ae86ff0f3e
github username <github email address>
2019-01-04 07:25:52 -0500

    rollback put names for original migration back and rollback purchases and items tables

--- db/migrate/20180614000947_create_items.rb
+++ db/migrate/20180614000947_create_items.rb
@@ -1,9 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name
-      t.decimal :price
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :items, foreign_key: true
       t.integer :quantity
+
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_14_000620) do
-
-  create_table "purchases", force: :cascade do |t|
-    t.string "name"
-    t.decimal "price"
-    t.integer "quantity"
-  end
+ActiveRecord::Schema.define(version: 2018_06_14_000124) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
