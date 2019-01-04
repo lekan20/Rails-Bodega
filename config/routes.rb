dcd046360bef7102eb7c7e4fb4a2a2c2d6fc8b3c
github username <github email address>
2019-01-04 18:58:49 -0500

    update routes to match updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'signin', to: "sessions#new"
   post "/sessions/create", to: "sessions#create"
-  post '/items/new', to: "items#new"
+  post '/user_items/new', to: "user_items#new"
   delete '/signout', to: "sessions#destroy"
   get '/auth/facebook/callback' => 'sessions#create'
   resources :users do
     resources :cart, only: [:index]
-    resources :purchases, only: [:new]
+    resources :items, only: [:new]
   end
-  resources :items, only: [:new, :edit, :update]
+  resources :user_items, only: [:new, :edit, :update]
   resources :purchases
 end
