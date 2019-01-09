fc0b68c60bc4927a6f72919be2acacfcdd346e31
github username <github email address>
2019-01-09 07:24:56 -0500

    Calling index page with fetch. Need to add API to the DOM

--- app/assets/javascripts/bodega.js
+++ app/assets/javascripts/bodega.js
@@ -1,22 +1,48 @@
-// $(function(){
-//   $("a.items-link").on("click", function(e){
-//     $.ajax({
-//       method: "GET",
-//       url: this.href,
-//       dataType: 'json'
-//     }).done(function(response) {
-//       let itemList = new List(response)
-//       debugger;
-//     })
-//     e.preventDefault()
-//   })
-// })
-//
-// class List {
-//   constructor(obj) {
-//     debugger;
-//   }
-// }
+$(function(){
+  $("a.items-link").on("click", function(e){
+    $.ajax({
+      method: "GET",
+      url: this.href,
+      dataType: 'json'
+    }).done(function(response) {
+      getItems()
+    })
+    e.preventDefault()
+  })
+})
+
+const getItems = () => {
+  fetch('/items.json')
+    .then(res => res.json())
+    .then(items => {
+      items.forEach((item) => {
+        let newList = new List(item)
+        // $("div.items-to-purchase").html(newList.listHTML()) - start here 1/10
+        debugger;
+      })
+    })
+}
+
+class List {
+  constructor(obj) {
+    this.id = obj.id
+    this.name = obj.name
+    this.price = obj.price
+    this.quantity = obj.quantity
+  }
+}
+
+List.prototype.listHTML = function() {
+    return (`
+      <li>${this.name}</li>
+      <li>${this.price}</li>
+      <li>${this.quantity}</li>
+    `)
+}
+
+
+
+
 
 $(function() {
   $("a.see_cart").on("click", function(e){
